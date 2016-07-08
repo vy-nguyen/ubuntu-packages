a10dca196fbd64db9a5f89fe4fe6f58b468278fb
Vy Nguyen <vynguyen@bienhoa>
2016-07-08 01:34:28 -0700

    Add script to install required packages.

--- script/install.sh
+++ script/install.sh
@@ -1,27 +1,65 @@
 #!/bin/bash
 
-sudo apt-get install -y openssh-client openssh-server
-sudo apt-get install -y git tree
-sudo apt-get install -y mysql-server make
+work_dir=`pwd`
+cp -rf etc /etc
+if [ "$?" -ne "0" ]; then
+    echo "You need to have root permission"
+    exit 1
+fi
+
+sudo apt-get install -y openssh-client openssh-server git tree make
+
+# Install Java
+#
+echo "Installing Java..."
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install -y oracle-java8-installer
+sudo ln -s /usr/lib/jvm/java-8-oracle/ /usr/lib/jvm/default-java 
+
+# Install node
+#
+echo "Installing node..."
+sudo apt-get remove --purge node
+curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh
+source ~/.profile
+nvm install 5.12.0
+n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
+npm install -g bower
+npm install -g webpack
+
+# Install gradle
+#
+echo "Installing Gradle..."
+wget https://downloads.gradle.org/distributions/gradle-2.14-bin.zip
+unzip gradle-2.14-bin.zip
+mv gradle-2.14 /usr/lib
+export GRADLE_HOME=/usr/lib/gradle-2.14
+rm /usr/bin/gradle
+ln -s /usr/lib/gradle-2.14/bin/gradle /usr/bin/gradle
+
+# Install mysql
+#
+sudo apt-get install -y mysql-server
 
 # password tienV13tNam@do
 
-mysql --user=root -p
+mysql --user=root -p < socnet.sql
 # create user 'socnet'@'localhost' identified by 'socnetsocnet';
 # GRANT ALL PRIVILEGES ON *.* TO 'socnet'@'localhost'; 
 # FLUSH PRIVILEGES;
 
+# Create ws clone
+#
 cd
 mkdir -p ws
 cd ws
+
 git clone https://github.com/vy-nguyen/tvntd.git tvntd
 
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install -y oracle-java8-installer
-
-sudo apt-get remove --purge node
-sudo ln -s /usr/lib/jvm/java-8-oracle/ /usr/lib/jvm/default-java 
+cd tvntd/java/socnet
+npm install
+bower install
 
 # Download gradle
 # unzip, install to /usr/lib
@@ -31,11 +69,10 @@ sudo ln -s /usr/lib/jvm/java-8-oracle/ /usr/lib/jvm/default-java
 
 # Install nvm
 #
-curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh
-source ~/.profile
-nvm install 5.12.0
-npm install
-npm install -g bower
-npm install -g webpack
+#curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh
+#source ~/.profile
+#nvm install 5.12.0
+#npm install
+#npm install -g bower
+#npm install -g webpack
 
-n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local
---
+++ script/socnet.sql
@@ -0,0 +1,4 @@
+create user 'socnet'@'localhost' identified by 'socnetsocnet';
+GRANT ALL PRIVILEGES ON *.* TO 'socnet'@'localhost';
+FLUSH PRIVILEGES;
+exit
